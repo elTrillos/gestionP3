62bfd02ee82b49ae1b6d86aeaeb06a7e97efdacb
Gonzalo Caballero <gacaballero@miuandes.cl>
2023-10-11 22:49:38 -0300

    fix flickering, closing and numeric values selection

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import math
 import pygame
 import random
@@ -18,22 +19,28 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 def render_launch(proyectile_path, obstacle,player1,player2):
     for coords in proyectile_path:
         win.fill((0, 0, 0))
-        pygame.display.update()
+
         pygame.draw.rect(win, (0, 255, 0), (FLOOR_START, HEIGHT-FLOOR_HEIGHT, WIDTH-FLOOR_START*2, FLOOR_HEIGHT))
+
         player1.draw(win)
         player2.draw(win)
+
         pygame.draw.circle(win, PROJECTILE_COLOR, (int(coords[0]+FLOOR_START), int(HEIGHT-FLOOR_HEIGHT - coords[1])), 5)
         obstacle.draw(win)
+
         pygame.display.flip()
         clock.tick(60)
 
 def render_all(obstacle,player1,player2):
     win.fill((0, 0, 0))
+
     pygame.display.update()
     pygame.draw.rect(win, (0, 255, 0), (FLOOR_START, HEIGHT-FLOOR_HEIGHT, WIDTH-FLOOR_START*2, FLOOR_HEIGHT))
+
     player1.draw(win)
     player2.draw(win)
     obstacle.draw(win)
+
     pygame.display.flip()
 
 def render_victory(player):
@@ -43,6 +50,7 @@ def render_victory(player):
     textRect = text.get_rect()
     textRect.center = (WIDTH // 2, HEIGHT // 2)
     win.blit(text, textRect)
+
     pygame.display.flip()
     sleep(5)
 
@@ -76,6 +84,7 @@ def draw_buttons():
     pygame.draw.rect(win, BLACK, (150, 200, 100, 40), 2)
     start_text = font.render("Start", True, BLACK)
     win.blit(start_text, (170, 205))
+
     # Update the display
     pygame.display.flip()
 
@@ -91,8 +100,14 @@ def input_text(input_count, input_text):
     active = False
     text = font.render(input_text, True, WHITE)
     win.blit(text, (50, 50))
+
     while True: 
         for event in pygame.event.get(): 
+            if event.type == pygame.QUIT:
+                running=False
+                pygame.quit()
+                sys.exit()
+
             if event.type == pygame.MOUSEBUTTONDOWN: 
                 if input_rect.collidepoint(event.pos): 
                     active = True
@@ -103,13 +118,13 @@ def input_text(input_count, input_text):
                 if event.key == pygame.K_BACKSPACE: 
                     user_text = user_text[:-1] 
 
-                elif event.key == 13:
-                    input_count += 1
+                elif event.key == 13: 
                     if user_text.isnumeric():
                         if float(user_text) < 0:
                             text = font.render(f"Enter a positive value", True, (255,0,0))
                             win.blit(text, (50, 100))
                         else:
+                            input_count += 1
                             return input_count, float(user_text)
                     else:
                         text = font.render(f"Enter a numeric value", True, (255,0,0))
@@ -125,16 +140,21 @@ def input_text(input_count, input_text):
         # set width of textfield so that text cannot get 
         # outside of user's text input 
         input_rect.w = max(100, text_surface.get_width()+10)
+
         pygame.display.flip()
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Proyectile Motion")
 selecting_difficulties = True
+
 while selecting_difficulties:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            running=False
+            pygame.quit()
+            sys.exit()
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = event.pos
             if 400 <= x <= 550 and 45 <= y <= 85:
@@ -151,6 +171,7 @@ while selecting_difficulties:
             
 
     draw_buttons()
+
 # Input values
 wind_params = {'None':[0,0], 'Easy':[0.25, 2.5], 'Medium':[2.5, 7.5], 'Hard':[7.5, 12.5]}
 obstacle_params = {'None':[0,0], 'Easy':[50, 100], 'Medium':[100, 150], 'Hard':[150, 200]}
@@ -167,33 +188,45 @@ obs=Obstacle(random.randint(250,350),random.randint(0,50),
 turn=0
 currentPlayer=1
 running = True
+
 # Calculate the trajectory and print positions
 #projectile_path=proyectile.calculate_trajectory(angle_deg, initial_velocity, initial_x, 0.01, time_step, gravity, wind_acceleration, obstacle)
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running=False
+            pygame.quit()
+            sys.exit()
+
     if currentPlayer==1:
         state=0
         get_inputs = True
         input_count = 0
+
         while get_inputs:
             input_count, angle_deg = input_text(input_count, "P1 -> Enter the launch angle (degrees): ")
             input_count, initial_velocity = input_text(input_count, "P1 -> Enter the initial velocity (m/s):")
             if input_count == 2:
                 get_inputs = False
+
         render_all(obs,p1,p2)
+
         [projectile_path,state]=Proyectile.calculate_trajectory(angle_deg, initial_velocity, p1.x, 0.01, time_step, gravity, wind_acceleration, obs,p2)              
+
         render_launch(projectile_path, obs,p1,p2)
+
         if state==0:
             print("Projectile has reached the ground.")
+
         elif state==1:
             print("Projectile has reached the obstacle.")
+
         elif state==2:
             print("Projectile has reached the oponent.")
             render_victory(1)
             break
         currentPlayer=2
+    
     elif currentPlayer==2:
         state=0
         get_inputs = True
@@ -203,17 +236,24 @@ while running:
             input_count, initial_velocity = input_text(input_count, "P2 -> Enter the initial velocity (m/s):")
             if input_count == 2:
                 get_inputs = False
+
         render_all(obs,p1,p2)
+
         [projectile_path,state]=Proyectile.calculate_trajectory(-angle_deg+180, initial_velocity, p2.x, 0.01, time_step, gravity, wind_acceleration, obs,p1)              
+        
         render_launch(projectile_path, obs,p1,p2)
+
         if state==0:
             print("Projectile has reached the ground.")
+
         elif state==1:
             print("Projectile has reached the obstacle.")
+
         elif state==2:
             print("Projectile has reached the oponent.")
             render_victory(2)
             break
+
         currentPlayer=1
     
     
